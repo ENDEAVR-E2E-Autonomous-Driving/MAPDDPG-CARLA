d67eeef2ef02277cec2ff04443cc6c614953bd3d
Dylan Nguyen <dylan.nguyen39@gmail.com>
2024-03-28 13:02:15 -0500

    change network architecture, no pooling

--- networks.py
+++ networks.py
@@ -5,21 +5,23 @@ import torch
 import numpy as np
 import cv2
 
+"""
+Critic Network
+"""
 class Critic(nn.Module):
     def __init__(self):
         super(Critic, self).__init__()
 
-        # Convolution and pooling layers
+        # Convolution layers (no pooling)
         # conv2d(in_channels (3 for rgb), out_channels (number of filters), kernel_size (filter size), stride)
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=8, stride=4)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2)
+        self.conv5 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2)
 
         # Fully connected layers
-        self.flatten_size = 128*6*8 # last 3 dimensions of pooling layer multiplied together (flattening)
+        self.flatten_size = 128*13*18 # last 3 dimensions of pooling layer multiplied together (flattening)
         self.fc1 = nn.Linear(self.flatten_size, 512)
         self.fc2 = nn.Linear(512, 416) # last FC layer is 416 to accomodate concatenation with other representations
 
@@ -41,9 +43,11 @@ class Critic(nn.Module):
 
         # Pass state image through conv and pooling layers
         # Shape of each layer output follows format: [batch_size, num channels, height, width]
-        x = F.relu(self.pool1(self.conv1(state)))
-        x = F.relu(self.pool2(self.conv2(x)))
-        x = F.relu(self.pool3(self.conv3(x)))
+        x = F.relu(self.conv1(state))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
 
         # Flatten and pass through fully connected layers
         x = x.reshape(x.size(0), -1) # flatten by keeping the batch dim and transforming features: channels*height*width
@@ -67,6 +71,12 @@ class Critic(nn.Module):
         Q = self.q_value_stream(x)
 
         return Q
+
+
+"""
+Actor Network with 
+"""
+
     
 
 if __name__ == '__main__':
