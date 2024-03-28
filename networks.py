24f0394b07d47de83cfd2ef4e9dc2e8ac0bf3dd0
Dylan Nguyen <dylan.nguyen39@gmail.com>
2024-03-28 12:02:40 -0500

    adjusted layers

--- networks.py
+++ networks.py
@@ -6,13 +6,12 @@ import numpy as np
 import cv2
 
 class Critic(nn.Module):
-    def __init__(self, action_dim):
+    def __init__(self):
         super(Critic, self).__init__()
 
         # Convolution and pooling layers
         # conv2d(in_channels (3 for rgb), out_channels (number of filters), kernel_size (filter size), stride)
-
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=6, stride=4)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=8, stride=4)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -20,16 +19,54 @@ class Critic(nn.Module):
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # Fully connected layers
-        ...
+        self.flatten_size = 128*6*8 # last 3 dimensions of pooling layer multiplied together (flattening)
+        self.fc1 = nn.Linear(self.flatten_size, 512)
+        self.fc2 = nn.Linear(512, 416) # last FC layer is 416 to accomodate concatenation with other representations
+
+        # Fully connected layers for vehicle ego state
+        self.v_fc1 = nn.Linear(29, 128)
+        self.v_fc2 = nn.Linear(128, 64)
+
+        # Fully connected layers for actions
+        self.a_fc1 = nn.Linear(3, 128)
+        self.a_fc2 = nn.Linear(128, 32)
+        
+        # FC Layers with features from fc2 concatenated with vehicle ego state and actions from the actor network
+        self.fc3 = nn.Linear(512, 512) 
+        self.fc4 = nn.Linear(512, 512)
+        self.q_value_stream = nn.Linear(512, 1) # returns expected total reward for current state-action pair
     
-    def forward(self, state):
+    def forward(self, state, vehicle_ego_state, actions):
         state = state.float() / 255.0
 
+        # Pass state image through conv and pooling layers
+        # Shape of each layer output follows format: [batch_size, num channels, height, width]
         x = F.relu(self.pool1(self.conv1(state)))
         x = F.relu(self.pool2(self.conv2(x)))
         x = F.relu(self.pool3(self.conv3(x)))
 
-        return x
+        # Flatten and pass through fully connected layers
+        x = x.reshape(x.size(0), -1) # flatten by keeping the batch dim and transforming features: channels*height*width
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2)
+
+        # Pass vehicle state through their respective fc layers
+        vehicle_x = F.relu(self.v_fc1(vehicle_ego_state))
+        vehicle_x = F.relu(self.v_fc2(vehicle_x))
+
+        # Pass actions through their respective fc layers
+        action_x = F.relu(self.a_fc1(actions))
+        action_x = F.relu(self.a_fc2(action_x))
+
+        # Concatenate features from FC layers with vehicle ego state and actor actions, and pass through final FC layers
+        x = torch.cat((x, vehicle_x, action_x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+
+        # Q value stream
+        Q = self.q_value_stream(x)
+
+        return Q
     
 
 if __name__ == '__main__':
