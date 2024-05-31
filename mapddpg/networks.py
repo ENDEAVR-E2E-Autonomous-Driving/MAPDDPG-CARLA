ea78053a2e14eac8a077822646daf73c05cc0fe8
Dylan Nguyen <90433391+dnguyencodez@users.noreply.github.com>
2024-05-31 17:50:44 -0500

    Update networks.py

--- mapddpg/networks.py
+++ mapddpg/networks.py
@@ -244,6 +244,8 @@ class Actor(nn.Module):
         x = F.relu(self.conv4(x))
         x = F.relu(self.conv5(x))
 
+        print(x.size())
+
         # apply CBAM
         x = x.view(batch_size, seq_len, 128, 8, 18)
         x = self.cbam(x)
